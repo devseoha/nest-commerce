26a386e957cc609b29e5c3110f764257d5b6e1a1
summer <devsh4030@gmail.com>
2023-01-30 16:08:23 +0900

    add. 테스트 코드 너무 어렵다..

--- src/domain/user/user.controller.spec.ts
+++ src/domain/user/user.controller.spec.ts
@@ -1,20 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from '../../entities/users.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from '../../libs/auth/local.strategy';
+import { JwtStrategy } from '../../libs/auth/jwt.strategy';
+import { ormConfig } from '../../../ormconfig';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule,
+        TypeOrmModule.forRoot(ormConfig),
+        TypeOrmModule.forFeature([Users]),
+        JwtModule.register({
+          secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+          signOptions: {
+            expiresIn: `${process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME}s`,
+          },
+        }),
+      ],
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, LocalStrategy, JwtStrategy],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
+    userController = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
   });
 });
--- src/domain/user/user.service.spec.ts
+++ src/domain/user/user.service.spec.ts
@@ -1,24 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from '../../../ormconfig';
 import { Users } from '../../entities/users.entity';
-import { JwtModule } from '@nestjs/jwt';
-import { BadRequestException } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { LocalStrategy } from '../../libs/auth/local.strategy';
+import { JwtStrategy } from '../../libs/auth/jwt.strategy';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UserService', () => {
-  let service: UserService;
-
-  class MockUserRepository {}
-  class MockBannerRepository {}
-  class MockBannerTargetRepository {}
-  class MockTargetRepository {}
+  let userService: UserService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         PassportModule,
+        TypeOrmModule.forRoot(ormConfig),
+        TypeOrmModule.forFeature([Users]),
         JwtModule.register({
           secret: process.env.JWT_ACCESS_TOKEN_SECRET,
           signOptions: {
@@ -26,56 +27,28 @@ describe('UserService', () => {
           },
         }),
       ],
-      providers: [
-        UserService,
-        {
-          provide: getRepositoryToken(Users),
-          useClass: MockUserRepository,
-        },
-      ],
+      controllers: [UserController],
+      providers: [UserService, LocalStrategy, JwtStrategy],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  describe('register', () => {
-    it('회원가입 시 email체크', async () => {
-      const data: CreateUserDto = {
-        email: 'mooo4030',
-        password: 'password1234',
-        birth: '19901130',
+  describe('signUp', () => {
+    it('should sign up a new user', async () => {
+      const userData: CreateUserDto = {
+        email: 'test@test.com',
+        password: 'Password123!',
+        birth: '19950101',
       };
 
-      try {
-        await expect(service.join(data)).resolves.toThrowError();
-      } catch (error) {
-        expect(error.message).toBe('Invalid email');
-      }
+      const expectedResult = { id: 1, accessToken: 'test-accesstoken' };
+      jest.spyOn(userService, 'join').mockResolvedValue(expectedResult);
+      const result = await userService.join(userData);
+
+      expect(result).toEqual(expectedResult);
+      expect(userService.join).toHaveBeenCalledWith(userData);
     });
-    //
-    //   it('비밀번호 총6자리 && 필수로 숫자1자리가 들어가야합니다.', async () => {
-    //     const data: CreateUserDto = {
-    //       email: 'mooo4030@naver.com',
-    //       password: 'password',
-    //       birth: '19901130',
-    //     };
-    //
-    //     try {
-    //       await service.join(data);
-    //     } catch (error) {
-    //       expect(error.message).toBe('Password must be at least 6 characters');
-    //     }
-    //   });
-    //
-    //   it('should successfully register a user', async () => {
-    //     const data: CreateUserDto = {
-    //       email: 'mooo4030@naver.com',
-    //       password: 'password',
-    //       birth: '19901130',
-    //     };
-    //
-    //     const registeredUser = await service.join(data);
-    //     expect(registeredUser.id).toBe('??');
-    //   });
   });
 });
